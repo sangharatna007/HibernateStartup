64415d10423b55c47d983e136e27be519c3e2243
Sangharatna <sangharatna007@gmail.com>
2018-07-29 11:34:08 +0530

    connection with mysql

---
+++ src/main/java/Employee.java
@@ -0,0 +1,31 @@
+
+public class Employee {
+
+    private int emp_id;
+    private String name;
+    private String last_name;
+
+    public int getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
